a0445c6317bb28986f56d6132010e58aa2c7a76d
lyle <lilyui@126.com>
2018-06-18 14:59:13 +0800

    update base class

--- schema/tables/base.py
+++ schema/tables/base.py
@@ -10,15 +10,21 @@ from sqlalchemy.ext.declarative import as_declarative
 @as_declarative()
 class BaseTable(object):
 
-    def toStr(self):
+    def toStr(self, blist=[]):
         s = deepcopy(self.__dict__)
         del(s['_sa_instance_state'])
+        for i in s:
+            if isinstance(s[i], datetime.datetime):
+                s[i] = str(s[i])
+        for b in blist:
+            if b in s:
+                del s[b]
         return s
 
     @classmethod
-    def getAll(cls, db, toStr=False):
+    def getAll(cls, db, toStr=True, blist=[]):
         if toStr:
-            return [i.toStr() for i in db.query(cls).all()]
+            return [i.toStr(blist) for i in db.query(cls).all()]
         else:
             return db.query(cls).all()
 
@@ -33,5 +39,20 @@ class BaseTable(object):
     @classmethod
     def delete(cls, db, uid):
         obj = db.query(cls).filter(cls.uid == uid).delete()
+        db.commit()
+        return True
+
+    @classmethod
+    def add(cls, db, **kwargs):
+        o = cls()
+        for key in kwargs:
+            o.__setattr__(key, kwargs[key])
+        db.add(o)
+        db.commit()
+        return True
+
+    @classmethod
+    def update(cls, db, uid, **kwargs):
+        db.query(cls).filter(cls.uid == uid).update(kwargs)
         db.commit()
         return True
