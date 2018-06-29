5c295b8e2c8575880fda539cf388e8adb5c4be8f
lyle <lilyui@126.com>
2018-06-29 14:12:46 +0800

    fix user create bug

--- schema/sql.sql
+++ schema/sql.sql
@@ -5,14 +5,14 @@ CREATE DATABASE `test` DEFAULT CHARACTER SET utf8 collate utf8_general_ci;
 use `test`;
 
 CREATE TABLE `test` (
-    `uid` VARCHAR(32) NOT NULL,
+  `uid` VARCHAR(32) NOT NULL,
   `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`uid`)
 );
 
 
-CREATE USER 'test'@'localhost' IDENTIFIED BY 'random_password';
+CREATE USER IF NOT EXISTS 'test'@'localhost' IDENTIFIED BY 'random_password';
 GRANT all privileges ON test.* TO 'test'@'localhost';
 
-CREATE USER 'backup'@'localhost' IDENTIFIED BY 'another_random_password';
+CREATE USER IF NOT EXISTS 'backup'@'localhost' IDENTIFIED BY 'another_random_password';
 GRANT SELECT ON test.* TO 'backup'@'localhost';
